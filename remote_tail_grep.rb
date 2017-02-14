f4da06e7a3b5be9fe1d95c98e516924d230eee64
well <well@corp.globo.com>
2017-02-14 18:04:31 -0200

    More improvements

--- remote_tail_grep.rb
+++ remote_tail_grep.rb
@@ -4,31 +4,33 @@ require 'net/ssh'
 
 print "Qual o servidor/host que vc quer capturar os logs? "
 host = gets.chomp
+host.downcase!
 
 print "Qual tecnologia? (N)ginx ou (U)nicorn? "
 tech = gets.chomp
 tech.upcase!
+project = host.split('-')[0]
+layer = host.split('-')[2]
+
 
 if tech == "N"
   tech = "nginx"
-elsif tech == "U"
+elsif tech == "U" && layer == "fe"
   puts "FE não tem Unicorn"
+elsif tech == "U"
+  tech = "unicorn"
 else
   puts "Não conheço essa tecnologia. Tente novamente!"
 end
 
-project = host.split('-').first
-
-response_log = "/opt/logs/#{project}/#{tech}-fe/nginx-fe_response.log"
-access_log = "/opt/logs/#{project}/#{tech}-fe/nginx-fe_access.log"
-
+response_log = "/opt/logs/#{project}/#{tech}-#{layer}/nginx-fe_response.log"
+access_log = "/opt/logs/#{project}/#{tech}-#{layer}/#{tech}-#{layer}_access.log"
 
 
 def do_tail(session,file)
   session.open_channel do |channel|
     channel.on_data do |ch,data|
-      puts "[#{file}] ->
-	 #{data}"
+      puts "[#{file}] -> #{data}"
     end
     channel.exec "tail -f #{file}"
   end
