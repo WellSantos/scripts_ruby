cb006b9f96253530599ed447673981a0a64b3db5
well <well@corp.globo.com>
2017-02-14 16:19:47 -0200

    Add new features
    - ask which server to user
    - fill log path with part of name of host and technology

--- remote_tail_grep.rb
+++ remote_tail_grep.rb
@@ -1,13 +1,34 @@
+#!/usr/bin/env ruby
+
 require 'net/ssh'
 
-response_log = "/path/to/response_file.log"
-access_log = "/path/to/access_file.log"
-host = "host"
+print "Qual o servidor/host que vc quer capturar os logs? "
+host = gets.chomp
+
+print "Qual tecnologia? (N)ginx ou (U)nicorn? "
+tech = gets.chomp
+tech.upcase!
+
+if tech == "N"
+  tech = "nginx"
+elsif tech == "U"
+  puts "FE não tem Unicorn"
+else
+  puts "Não conheço essa tecnologia. Tente novamente!"
+end
+
+project = host.split('-').first
+
+response_log = "/opt/logs/#{project}/#{tech}-fe/nginx-fe_response.log"
+access_log = "/opt/logs/#{project}/#{tech}-fe/nginx-fe_access.log"
+
+
 
 def do_tail(session,file)
   session.open_channel do |channel|
     channel.on_data do |ch,data|
-      puts "[#{file}] -> #{data}"
+      puts "[#{file}] ->
+	 #{data}"
     end
     channel.exec "tail -f #{file}"
   end
